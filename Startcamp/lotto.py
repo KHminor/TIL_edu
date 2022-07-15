e4d273460eab58c821be806dee33092bcd5fc42d
KHminor <ghdals2020@gmail.com>
2022-07-15 17:47:47 +0900

    update

--- Startcamp/lotto.py
+++ Startcamp/lotto.py
@@ -88,51 +88,50 @@
 
 # 6. (도전과제) 1등부터 2등을 포함한 (보너스 번호까지) 4등까지의 각 당첨 횟수 출력하기.
 
-# import requests
-# import random
+import requests
+import random
 
-# num = input("회차번호: ")
-# url = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={}'.format(num)
-# response = requests.get(url).json()
-# drwtNo1 = response["drwtNo1"]
-# drwtNo2 = response["drwtNo2"]
-# drwtNo3 = response["drwtNo3"]
-# drwtNo4 = response["drwtNo4"]
-# drwtNo5 = response["drwtNo5"]
-# drwtNo6 = response["drwtNo6"]
-# li = [drwtNo1,drwtNo2,drwtNo3,drwtNo4,drwtNo5,drwtNo6]
-# li.sort()
-# print("금주차 {}의 로또 번호는 {} 입니다.".format(num,li))
-
-
-
-# a = 0
-# cnt = 0
-# while True :
-#     a += 1
-#     num_range = range(1,46)
-#     lucky = random.sample(num_range, 6)
-#     lucky.sort()
-#     if li == lucky:
-#         print("해당 번호 {}는 당첨되었습니다.".format(lucky))
-#         cnt += 1
-#     else:
-#         print(lucky)
-#     if a >= 1000:
-#         if cnt >= 1:
-#             print("{}회 로또에 당첨된 횟수는 {}번 입니다.".format(num,cnt))
-#         else:    
-#             print("{}회 로또에 당첨되지 못했습니다.".format(num))
-#         # print(a)
-#         break
-    
+num = input("회차번호: ")
+url = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={}'.format(num)
+response = requests.get(url).json()
+drwtNo1 = response["drwtNo1"]
+drwtNo2 = response["drwtNo2"]
+drwtNo3 = response["drwtNo3"]
+drwtNo4 = response["drwtNo4"]
+drwtNo5 = response["drwtNo5"]
+drwtNo6 = response["drwtNo6"]
+bnusNo = response["bnusNo"]
+li = [drwtNo1,drwtNo2,drwtNo3,drwtNo4,drwtNo5,drwtNo6,bnusNo]
+li.sort()
+print("금주차 {}의 로또 번호는 {} 입니다.".format(num,li))
 
 
-li = [1,2,3,4,5,6]
-li2 = [4,5,6,7,8,9]
-l_num = 0
+
+a = 0
+cnt = 0
+while True :
+    a += 1
+    num_range = range(1,46)
+    lucky = random.sample(num_range, 7) 
+    lucky.sort()
+    if li == lucky:
+        print("해당 번호 {}는 당첨되었습니다.".format(lucky))
+        cnt += 1
+    else:
+        print(lucky)
+    if a >= 1000:
+        if cnt >= 1:
+            print("{}회 로또에 당첨된 횟수는 {}번 입니다.".format(num,cnt))
+        else:    
+            print("{}회 로또에 당첨되지 못했습니다.".format(num))
+        # print(a)
+        break
+
+ 
 correct = []
 for i in range(len(li)):
-    for y in range(len(li2)):
-        if li[i] == li2[y]:
-            correct.append()
+    for y in range(len(lucky)):
+        if li[i] == lucky[y]:
+            correct.append(li[i])
+
+print(correct)
