caf52cb4d9e650e252600ffb6b1325b27a1a4563
KHminor <ghdals2020@gmail.com>
2022-07-18 18:54:12 +0900

    0718_OHW

---
+++ Startcamp/OHW_0718.py
@@ -0,0 +1,21 @@
+# score = {'python' : 80, 'django' : 89, 'wdb' : 83}
+
+# # 1.
+# score['algorithm'] = 90
+
+# # 2.
+# score['python'] = 85
+
+# # 3.
+# avg = sum(score.values())/ len(score.keys())
+# print(avg)
+
+# ====================================================
+
+# num = str(input("숫자를 입력하세요: "))
+# num = list(num)
+
+# sum = 0
+# for i in range(len(num)):
+#     sum += int(num[i])
+# print(sum)
