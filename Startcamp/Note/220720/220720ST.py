c7ba1ac6865575fa3330d715c859b7b5679f619d
KHminor <ghdals2020@gmail.com>
2022-07-20 19:46:10 +0900

    TIL_edu

---
+++ Startcamp/Note/220720/220720ST.py
@@ -0,0 +1,42 @@
+# dust =  80
+# if dust > 150:
+#     print("매우 나쁨")
+# elif dust > 80:
+#     print("나쁨")
+# elif dust > 30:
+#     print("보통")
+# else:
+#     print("좋음")
+
+
+
+# num = 2 
+# result = '홀수 입니다.' if num % 2 else '짝수 입니다.'
+
+# num = 5
+# values = num if num >= 0 else 0
+
+# a = {'john' : 80, 'eric' : 90}
+# for i, j in enumerate(a):
+#     print(i, j, a[j])
+# for i in a:
+#     print(a[i])
+# a = ['민수', '영희', '철수']
+# for i, j in enumerate(a):
+#     print(i, j)
+
+# a = ['민수', '영희', '철수']
+# enumerate(a)
+# print(list(enumerate(a)))
+# print(list(enumerate(a, start=1)))
+
+# a = [i **3 for i in range(1,4)]
+# print(a)
+
+# d = {f"{i}의 세제곱은" : i**3 for i in range(1, 4)}
+# print(d)
+
+# a = []
+# for i in range(1,4):
+#     a.append(i**3)
+# print(a)
