87b0ac0f867b6522d9b04d398032f1bf5491a622
KHminor <ghdals2020@gmail.com>
2022-09-06 23:16:45 +0900

    220906 | TIL_edu

---
+++ memo/Note/2209/220906/algorithm/a.py
@@ -0,0 +1,53 @@
+
+# T = int(input())
+# for tc in range(1,T+1):
+
+N = int(input())
+arr = [list(input()) for _ in range(N)]
+
+col_arr = [['']*N for _ in range(N)]
+
+for i in range(N):
+    for j in range(N):
+        col_arr[i][j] = arr[j][i]
+
+# row
+result = ''
+cross = 0
+r_cross = 0
+for i in range(N):
+    col = 0
+    row = 0
+
+    if arr[i][i] == 'o':
+        cross += 1
+    else:
+        cross = 0
+    if arr[i][N - 1 - i] == 'o':
+        r_cross += 1
+    else:
+        r_cross = 0
+    for j in range(N):
+        if arr[i][j] == 'o':
+            row += 1
+        else:
+            row = 0
+        if col_arr[i][j] == 'o':
+            col += 1
+        else:
+            col = 0
+    if row >= 5:
+        result = 'Yes'
+        break
+    elif col >= 5:
+        result = 'Yes'
+        break
+    elif cross >= 5:
+        result = 'Yes'
+        break
+    elif r_cross >= 5:
+        result = 'Yes'
+        break
+if result == '':
+    result = 'NO'
+print(result)
