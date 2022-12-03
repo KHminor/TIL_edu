4c9a1b9598f212cbcfd6708b29c604b3b72f3e4c
KHminor <ghdals2020@gmail.com>
2022-12-03 18:53:33 +0900

    react

--- Vacation/React_JS_Study/React/blog/src/App.js
+++ Vacation/React_JS_Study/React/blog/src/App.js
@@ -2,12 +2,9 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  let [글제목, 글제목함수] = useState(["코트", "아우터", "바지"]);
+  let [글제목, 글제목함수] = useState(["남자코트", "강남 우동맛집", "파이썬독학"]);
   let [글제목2, 글제목함수2] = useState(["221127", "221128", "221129"]);
-  // let [따봉, 따봉변경] = useState([0,0,0]);
-  let [따봉0, 따봉변경0] = useState(0);
-  let [따봉1, 따봉변경1] = useState(0);
-  let [따봉2, 따봉변경2] = useState(0);
+  let [따봉, 따봉변경] = useState([0,0,0]);
   
   let [성별, 성별변경] = useState(["여자", "남자"]);
   let [modal, setModal] = useState(false)
@@ -26,91 +23,29 @@ function App() {
         글제목.map((article,i)=> {
           return (
             <div>
-              <h1>{article} <span>👍</span>{따봉`${i}`}</h1>
+              <h1 onClick={()=> {setModal(true)}}>{article} <span style={{cursor: 'pointer'}} onClick={()=> {
+                let thumb = [...따봉]
+                thumb[i] = 따봉[i] +1
+                따봉변경(thumb)
+              }}>👍</span>{따봉[i]}</h1>
               <div>{글제목2[i]}</div>
             </div>
           )
         })
       }
 
-      {/* {
-        글제목.map((article,i)=> {
-          return (
-            <div>
-              <h1>{글제목[i]} <span onClick={()=> {
-                let up = [...따봉]
-                up[i] += 1
-                따봉변경(up)
-              }}>👍</span> {따봉[i]}</h1>
-              {글제목2[i]}
-            </div>
-          )
-        })
-      } */}
-    
-      {/* {
-        따봉.map((i)=> {return <div>{i}</div> })
-      } */}
-
-      {/* <div className="list">
-        <button onClick={제목바꾸기}>👩‍🦰</button>
-        <div style={{ textAlign: "start" }}>
-          <p style={{ fontSize: "1.5rem" }}>{성별[0]}코트 추천</p>
-          <p>2월 17일 발행</p>
-        </div>
-        <hr />
-        <div>
-          <button
-            onClick={() => {
-              let newArray = [...글제목];
-              newArray[0] = 글제목[2];
-              newArray[1] = 글제목[0];
-              newArray[2] = 글제목[1];
-              글제목함수(newArray);
-            }}
-          >
-            🐱‍🏍
-          </button>
-          <h1>
-            {글제목[0]}{" "}
-            <button
-              onClick={() => {
-                return 따봉변경(따봉 + 1);
-              }}
-            >
-              👍
-            </button>
-            {따봉}
-          </h1>
-          {글제목2[0]}
-        </div>
-        <hr />
-        <div>
-          <h1>{글제목[1]}</h1>
-          {글제목2[1]}
-        </div>
-        <hr />
-        <div>
-          <h1 onClick={() => {
-            modal == false ? setModal(true) : setModal(false)
-            }}>{글제목[2]}</h1>
-          {글제목2[2]}
-        </div>
-        <hr />
-      </div>
-
       {
-        modal== true ? <Modal/> : null
-      } */}
+        modal == true ? <Modal 글수정버튼={글제목함수} color="skyblue" bgc={'yellow'} 글제목={글제목}  /> : null
+      }
 
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
-    <div className="modal">
-      <h2>제목</h2>
+    <div className="modal" style={{backgroundColor:props.color}}>
+      <h2 id="title">{props.글제목[0]}</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
