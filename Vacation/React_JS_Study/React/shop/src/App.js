e3dd0d56ff5a73a10ee6482cd8eb1590069138e1
KHminor <ghdals2020@gmail.com>
2022-12-13 20:04:49 +0900

    update

--- Vacation/React_JS_Study/React/shop/src/App.js
+++ Vacation/React_JS_Study/React/shop/src/App.js
@@ -1,31 +1,78 @@
 import './App.css';
 import React ,{useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {nav, items} from './data.js'
+import { items} from './data.js'
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import comp from './newcomponent'
+import {Navbar,Nav} from 'react-bootstrap';
 
 
 function App() {
 
   let [item, setItems] = useState(items)
+  let navigate = useNavigate()
 
   return (
     <div className="App">
-      {/* navbar */}
-      {nav}
-      
-      <div className='main-bg'>
-        {/* 배경사진 */}
-      </div>
 
-      <div className='contain'>
-        <div></div>
-        {item.map((item) => {
-          return item
-        })}
-        <div></div>
-      </div>
+      {/* navbar */}
+      <Navbar id='navbar' variant="dark">
+        <div className="grid subfont">
+          <div className=''></div>
+          <Nav style={{color:'black',justifyContent: 'center',alignItems: 'center' }} href="#home"><span class="material-symbols-outlined" style={{marginRight:'3px'}}>menu</span>메뉴</Nav>
+          <div></div>
+          <Nav style={{color:'black',justifyContent: 'center',alignItems: 'center' }}  href="#features"><span class="material-symbols-outlined">search</span>검색</Nav>
+          <div></div>
+          <Nav className='mainfont' style={{color:'black',justifyContent: 'center',alignItems: 'center', fontSize:'2rem', letterSpacing:'5px', cursor: 'pointer' }} onClick={()=> {navigate('/')}}>LOUIS VUITTON</Nav>
+          <div></div>
+          <Nav style={{color:'black',justifyContent: 'center',alignItems: 'center', cursor: 'pointer' }}  onClick={()=> {navigate('/detail')}} >위시리스트</Nav>
+          <div></div>
+          <Nav style={{color:'black',justifyContent: 'center',alignItems: 'center', cursor: 'pointer' }}  onClick={()=>{navigate('/about')}}>My lv</Nav>
+          <div></div>
+          <Nav style={{color:'black',justifyContent: 'center',alignItems: 'center' }}  href="#pricing"><span class="material-symbols-outlined">shopping_cart_checkout</span></Nav>
+        </div>
+      </Navbar>
+      
+      {/* <Link style={{textDecoration: 'none', color:'black'}} to={'/'}>홈 </Link><br />
+      <Link to={'/detail'}>상세페이지</Link><br />
+      <Link to={'/comp'}>새로운 컴포넌트</Link> */}
+
+      <Routes>
+        <Route path='/' element={<>
+        <div className='main-bg'>
+        {/* 배경사진 */}
+        </div></>} />
+
+        <Route path='/detail' element={<>
+          <div className='contain'>
+            <div></div>
+            {item.map((item) => {
+              return item})}
+            <div></div>
+          </div></>}/>
+        <Route path='/comp' element={<>
+          {comp}
+        </>}/>
+          <Route path='event' element={<> <Event /></>}>
+            <Route path='one' element={<><p>첫 주문시 양배추즙 서비스</p></>}/>
+            <Route path='two' element={<><p>생일기념 쿠폰받기</p></>}/>
+          </Route>
+
+        <Route path='*' element={<>여긴 뭐하러 왔어?</>}/>
+      </Routes>
+
     </div>
   );
 }
+
+function Event() {
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
 
 export default App;
--- Vacation/React_JS_Study/React/shop/src/data.js
+++ Vacation/React_JS_Study/React/shop/src/data.js
@@ -1,25 +1,3 @@
-import {Navbar,Nav} from 'react-bootstrap';
-
-let nav = [
-  <Navbar id='navbar' variant="dark">
-        <div className="grid subfont">
-          <div className=''></div>
-          <Nav style={{color:'black',justifyContent: 'center',alignItems: 'center' }} href="#home"><span class="material-symbols-outlined" style={{marginRight:'3px'}}>menu</span>메뉴</Nav>
-          <div></div>
-          <Nav style={{color:'black',justifyContent: 'center',alignItems: 'center' }}  href="#features"><span class="material-symbols-outlined">search</span>검색</Nav>
-          <div></div>
-          <Nav className='mainfont' style={{color:'black',justifyContent: 'center',alignItems: 'center', fontSize:'2rem', letterSpacing:'5px'}}  href="#home" >LOUIS VUITTON</Nav>
-          <div></div>
-          <Nav style={{color:'black',justifyContent: 'center',alignItems: 'center' }}  href="#home" >위시리스트</Nav>
-          <div></div>
-          <Nav style={{color:'black',justifyContent: 'center',alignItems: 'center' }}  href="#pricing">My lv</Nav>
-          <div></div>
-          <Nav style={{color:'black',justifyContent: 'center',alignItems: 'center' }}  href="#pricing"><span class="material-symbols-outlined">shopping_cart_checkout</span></Nav>
-        </div>
-      </Navbar>
-]
-
-
 let datas = [
   {
     id : 0,
@@ -61,4 +39,4 @@ let items = [
   </div>
 ]
 
-export {nav, items, datas}
+export {items, datas}
