4de9d826da5b3bd6d08eba04debbbe7d83cecf3e
KHminor <ghdals2020@gmail.com>
2022-12-16 21:34:09 +0900

    1216

--- Vacation/React_JS_Study/React/shop/src/Detail.js
+++ Vacation/React_JS_Study/React/shop/src/Detail.js
@@ -1,19 +1,35 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import {datas} from './data'
 
 function Detail(props) {
 
-  let {id} = useParams()
+  let [alert, setAlert] = useState(true) 
 
+  useEffect(()=> {
+    setTimeout(() => {
+      setAlert(false)}, 2000);
+  }, [])
+
+  let {id} = useParams()
+  let [num, setNum] = useState(0)
   let data = props.datas.find((data)=> {
-    console.log(data);
+    // console.log(data);
     return data.id === Number(id)
   })
 
   let dataId = data.id+1
 
   return (
-    <div style={{display:'flex', justifyContent:'center',alignItems:'center'}}>
+    <div style={{display:'f lex', justifyContent:'center',alignItems:'center'}}>
+      {
+        alert === true 
+        ? <div id="sale" className="alert alert-warning">
+            2초이내 구매시 할인
+          </div>
+        : null
+      }
+      <button onClick={()=> {setNum(num+1)}}>{num}</button>
       <div style={{marginRight:'4px'}}>
         <img className='items' style={{height:'50%'}} src={'https://codingapple1.github.io/shop/shoes' + dataId +'.jpg'} alt="" />
         <p style={{marginTop:'7px'}}>{datas[data.id].title}</p>
