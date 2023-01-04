c5642adaa7e104a35aaf4a92aa406b9bf7529440
KHminor <ghdals2020@gmail.com>
2023-01-05 00:39:48 +0900

    update

--- 2nd/typeScript/quest.js
+++ 2nd/typeScript/quest.js
@@ -1,12 +1,34 @@
-// 1번
-var 이름 = '홍민';
-var 나이 = 30;
-var 출생지역 = '울산';
-// 2번
-var like = { name: 'sabrina claudio', songName: 'Come Here' };
-// 3번
-var project = {
-    member: ['kim', 'park'],
-    days: 30,
-    started: true,
+// // 1번
+// let 이름 = '홍민'
+// let 나이 = 30
+// let 출생지역 = '울산'
+// // 2번
+// let like = {name :'sabrina claudio', songName : 'Come Here'}
+// // 3번
+// let project :{
+//   member : string[],
+//   days : number,
+//   started : boolean,
+// } = {
+//   member : ['kim', 'park'],
+//   days : 30,
+//   started : true,
+// }
+// let 리스트 :(string | number)[] = [1,'2',3]
+// let 오브젝트 :{[key:string]: (string | number) } = {a:'123', b:123}
+// let 아무거나 :any
+// let 나이 : number | string
+// 나이 + 1
+// 숙제 1
+var user = 'kim';
+var age = undefined;
+var married = false;
+var 철수 = [user, age, married];
+// 숙제 2
+var 학교 = {
+    score: [100, 97, 84],
+    teacher: 'Phil',
+    friend: 'John'
 };
+학교.score[4] = false;
+학교.friend = ['Lee', 학교.teacher];
--- 2nd/typeScript/quest.ts
+++ 2nd/typeScript/quest.ts
@@ -1,20 +1,47 @@
-// 1번
-let 이름 = '홍민'
-let 나이 = 30
-let 출생지역 = '울산'
+// // 1번
+// let 이름 = '홍민'
+// let 나이 = 30
+// let 출생지역 = '울산'
 
-// 2번
-let like = {name :'sabrina claudio', songName : 'Come Here'}
+// // 2번
+// let like = {name :'sabrina claudio', songName : 'Come Here'}
 
-// 3번
+// // 3번
+// let project :{
+//   member : string[],
+//   days : number,
+//   started : boolean,
+// } = {
+//   member : ['kim', 'park'],
+//   days : 30,
+//   started : true,
+// }
+// let 리스트 :(string | number)[] = [1,'2',3]
+// let 오브젝트 :{[key:string]: (string | number) } = {a:'123', b:123}
+
+// let 아무거나 :any
+
+// let 나이 : number | string
+// 나이 + 1
 
 
-let project :{
-  member : string[],
-  days : number,
-  started : boolean,
+
+
+// 숙제 1
+let user :string = 'kim';
+let age :undefined | number = undefined;
+let married :boolean = false; 
+let 철수 :(string | undefined | number | boolean)[] = [user, age, married];
+
+// 숙제 2
+let 학교 :{
+  score : (number|boolean)[],
+  teacher? : string,
+  friend : string |(string)[]
 } = {
-  member : ['kim', 'park'],
-  days : 30,
-  started : true,
+  score : [100, 97, 84],
+  teacher : 'Phil',
+  friend : 'John'
 }
+학교.score[4] = false;
+학교.friend = ['Lee' , 학교.teacher]
