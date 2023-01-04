bb3c7dd0ba52deb3cd4f0f6e4340496dbe3242d0
KHminor <ghdals2020@gmail.com>
2023-01-04 23:39:19 +0900

    ts

---
+++ 2nd/typeScript/index.js
@@ -0,0 +1,2 @@
+var john = { name: 'kim', home: 'vilia' };
+var abc = { name: 1, home: 2 };
---
+++ 2nd/typeScript/index.ts
@@ -0,0 +1,10 @@
+type Member = {
+  [key :string] :string
+}
+
+type Count = {
+  [key :string] :number
+}
+
+let john :Member = {name :'kim', home :'vilia'}
+let abc :Count = {name: 1, home:2}
