69753edb1318b738b6b29a9ef6a866b7bd0187b2
KHminor <ghdals2020@gmail.com>
2023-01-19 15:47:25 +0900

    update

---
+++ 2nd/script2/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+let 박스 : JSX.Element  = <div>hi</div>
+function App() {
+  return (
+    <div className="App">
+      {박스}
+      {박스}
+      {박스}
+    </div>
+  );
+}
+
+export default App;
---
+++ 2nd/typeScript/sanghyun.ts
@@ -0,0 +1,8 @@
+function 함수(x :string | number) {
+  if (typeof(x) === 'number') {
+    console.log(x+1);
+
+  }
+  
+}
+함수(1)
