4f9dd89243cb763804886725d7ee5424ced1e459
KHminor <ghdals2020@gmail.com>
2023-02-27 23:46:27 +0900

    update

--- 2nd/Three/threejs/src/App.js
+++ 2nd/Three/threejs/src/App.js
@@ -1,36 +1,39 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import * as THREE from 'three'
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import Character from './Character.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 function App() {
+  const canvas = useRef()
+  // 1. 장면 만들기 
+  let scene = new THREE.Scene()
+  // 2. 브라우저에 렌더링
+  // 3D 요소를 웹 브라우저에 보여주려고 할 경우 WebGL 를 사용한다고 함 
+  let renderer = new THREE.WebGLRenderer({
+    // 웹 브라우저 어디에 보여줄지는 여기서 지정
+    canvas: document.getElementById('canvas')
+  })
 
-  const [click, setClick] = useState(false)
-  const [hover, setHover] = useState(false)
+  // 3D model을 보여줄 때 필요한 것들
+  // 1.카메라, 2.조명, 3.배경
+  
+  //  PerspectiveCamera = 원근법, OrthographicCamera = 원근법 X
+  let camera = new THREE.PerspectiveCamera(30,1)
+
+  // 다운받은 것을(gltf파일의 경우) 보여주고자 할 경우
+  let loader = new GLTFLoader()
+  loader.load('scene.gltf', function(gltf) {
+    scene.add(gltf.scene)
+    renderer.render(scene, camera)
+  })
   return (
     <div className="App">
-      <Canvas>
-        <OrbitControls autoRotate={true}/>
-        <mesh onClick={()=> {setClick(!click)}} 
-              onPointerOver={()=> {setHover(!hover)}}
-              onPointerOut={()=> {setHover(!hover)}}
-              scale={hover? 1.5: 1}
-              
-            >
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-          <ambientLight intensity={1}/>
-          <pointLight position={[10, 10, 10]} />
-          <boxGeometry  args={[1, 1, 1]}/>
-          <meshStandardMaterial attach="material" color={click? 'pink': 0xa3b18a}
-            
-          />
-        </mesh>
-        {/* <mesh rotation={[90, 90, 8]}>
-          <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-          <meshLambertMaterial attach="material" color="blue" />
-        </mesh> */}
-      </Canvas>
+      <canvas ref={canvas} id='canvas' width={300} height={300}>
+      
+      </canvas>
     </div>
   );
 }
---
+++ 2nd/Three/threejs/src/Character.js
@@ -0,0 +1,43 @@
+import * as THREE from 'three'
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { useRef } from 'react';
+
+
+function Character() {
+  const canvas = useRef()
+  console.log(canvas.current);
+  // 1. 장면 만들기 
+  let scene = new THREE.Scene()
+  // 2. 브라우저에 렌더링
+  // 3D 요소를 웹 브라우저에 보여주려고 할 경우 WebGL 를 사용한다고 함 
+  let renderer = new THREE.WebGLRenderer({
+    // 웹 브라우저 어디에 보여줄지는 여기서 지정
+    canvas: document.querySelector('#canvas')
+  })
+
+  // 3D model을 보여줄 때 필요한 것들
+  // 1.카메라, 2.조명, 3.배경
+  
+  //  PerspectiveCamera = 원근법, OrthographicCamera = 원근법 X
+  let camera = new THREE.PerspectiveCamera(30,1)
+
+  // 다운받은 것을(gltf파일의 경우) 보여주고자 할 경우
+  let loader = new GLTFLoader()
+  loader.load('scene.gltf', function(gltf) {
+    scene.add(gltf.scene)
+    renderer.render(scene, camera)
+  })
+
+  
+
+  return (
+    <>
+      <canvas ref={canvas} id='canvas' style={{width: '300', height: '300'}}>
+      
+      </canvas>
+    </>
+  )
+}
+export default Character
