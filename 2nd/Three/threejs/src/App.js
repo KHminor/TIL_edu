a1cee4d23f01bb8be571ab04478643951ad932c2
KHminor <ghdals2020@gmail.com>
2023-02-28 17:53:50 +0900

    update

--- 2nd/Three/threejs/src/App.js
+++ 2nd/Three/threejs/src/App.js
@@ -1,41 +1,48 @@
-import './App.css';
-import React, { useRef, useState } from 'react';
-import * as THREE from 'three'
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import Character from './Character.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import * as THREE from 'three';
+import { useEffect, useRef } from 'react';
 
-function App() {
-  const canvas = useRef()
-  // 1. 장면 만들기 
-  let scene = new THREE.Scene()
-  // 2. 브라우저에 렌더링
-  // 3D 요소를 웹 브라우저에 보여주려고 할 경우 WebGL 를 사용한다고 함 
-  let renderer = new THREE.WebGLRenderer({
-    // 웹 브라우저 어디에 보여줄지는 여기서 지정
-    canvas: document.getElementById('canvas')
-  })
+function MyCanvas() {
+  const canvasRef = useRef(null);
 
-  // 3D model을 보여줄 때 필요한 것들
-  // 1.카메라, 2.조명, 3.배경
-  
-  //  PerspectiveCamera = 원근법, OrthographicCamera = 원근법 X
-  let camera = new THREE.PerspectiveCamera(30,1)
+  useEffect(() => {
+    let scene = new THREE.Scene();
+    let renderer = new THREE.WebGLRenderer({
+      canvas: canvasRef.current,
+      antialias: true,
+    });
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    let camera = new THREE.PerspectiveCamera(30, 1);
+    camera.position.set(0, 0, 1000);
+    scene.background = new THREE.Color('#c9f0ff');
+    let light = new THREE.DirectionalLight(0xffffff, 1.4);
+    let light1 = new THREE.AmbientLight(0xffffff);
+    light.position.set(100, 50, 50);
+    scene.add(light, light1);
 
-  // 다운받은 것을(gltf파일의 경우) 보여주고자 할 경우
-  let loader = new GLTFLoader()
-  loader.load('scene.gltf', function(gltf) {
-    scene.add(gltf.scene)
-    renderer.render(scene, camera)
-  })
+    let loader = new GLTFLoader();
+    loader.load('pusheen/scene.gltf', function (gltf) {
+      scene.add(gltf.scene);
+      function animate() {
+        requestAnimationFrame(animate);
+        gltf.scene.rotation.y -= 0.01;
+        renderer.render(scene, camera);
+      }
+      animate();
+    });
+  }, []);
+
+  return <canvas ref={canvasRef} width={400} height={400} />;
+}
+
+export default function App() {
   return (
-    <div className="App">
-      <canvas ref={canvas} id='canvas' width={300} height={300}>
-      
-      </canvas>
+    <div>
+      <MyCanvas />
     </div>
   );
 }
 
-export default App;
+
+
+
