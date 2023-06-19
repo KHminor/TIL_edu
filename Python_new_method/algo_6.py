11c8bc7de44574ae8522e8669d8ecd1e9f68a7f7
KHminor <ghdals2020@gmail.com>
2023-06-19 10:20:49 +0900

    algo

---
+++ Python_new_method/algo_6.py
@@ -0,0 +1,14 @@
+def solution(k, tangerine):
+    dic, cnt = {}, 0
+    for i in tangerine:
+        try: dic[i] += 1
+        except: dic[i] = 1
+    dic = sorted(dic.items(), key=lambda x:x[1], reverse=True)
+    for i in range(len(dic)):
+        k -= dic[i][1]
+        cnt += 1
+        if k <= 0: break
+    return cnt
+
+
+solution(6, [1, 3, 2, 5, 4, 5, 2, 3])
---
+++ Python_new_method/algo_8.py
@@ -0,0 +1,6 @@
+def solution(citations):
+    h, c_ln, result = 0, len(citations), 0
+    for i in range(1, c_ln+1):
+        ln = len([j for j in citations if j >= i])
+        if not (ln >= i): return result
+        elif ln >= i: result = i
