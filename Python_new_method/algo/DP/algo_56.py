f35b64a48defa03a84577c4eb9831e175a988f90
KHminor <ghdals2020@gmail.com>
2023-08-02 12:23:48 +0900

    algo

---
+++ Python_new_method/algo/DP/algo_56.py
@@ -0,0 +1,16 @@
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+for i in range(int(input())):
+    n,m = map(int,input().split())
+    print(len(list(combinations([i for i in range(m)],n))))
+
+# 조합의 공식으로 nCm의 경우엔
+# n!/(n-m)!*m!
+
+# import sys
+# from math import factorial
+# input = sys.stdin.readline
+# for _ in range(int(input())):
+#     n,m = map(int,input().split())
+#     print(int(factorial(m)/(factorial(m-n)*factorial(n))))
