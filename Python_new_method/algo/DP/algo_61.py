ad94586493ab56f2a75e2efbe42c72f23b2eb0be
KHminor <ghdals2020@gmail.com>
2023-08-06 13:38:12 +0900

    algo

--- Python_new_method/algo/DP/algo_61.py
+++ Python_new_method/algo/DP/algo_61.py
@@ -1,23 +1,22 @@
-a, b = input(), input()
-if len(b) > len(a):
-    tmp = a
-    a = b
-    b = tmp
-dp = [0]*len(b)
+# 2차원 캐시를 통해 푸는 방법
+a,b = input(), input()
+ln_a, ln_b = len(a), len(b)
+dp = [[0]*(ln_b+1) for _ in range(ln_a+1)]
+for i in range(1, ln_a+1):
+    for j in range(1, ln_b+1):
+        if a[i-1] == b[j-1]: dp[i][j] = dp[i-1][j-1]+1
+        else: dp[i][j] = max(dp[i-1][j], dp[i][j-1]) 
+print(max(dp[-1]))
 
-for x in range(len(b)):
-    state = 0
-    for i in range(x, len(b)):
-        if i == 0:
-            for j in range(i, len(a)):
-                if b[i] == a[j]:
-                    dp[x] +=1
-                    break
-            if j == len(a)-1: break
-        else:
-            for j in range(i+1, len(a)):
-                if b[i] == a[j]:
-                    dp[x] +=1
-                    break
-            if j == len(a)-1: break
-# 틀린 코드임
+# 1차원 캐시를 통해 누적하는 방법
+a,b = input(), input()
+ln_a, ln_b = len(a), len(b)
+dp = [0]*ln_b
+for i in range(ln_a):
+    cnt = 0
+    for j in range(ln_b):
+        if dp[j] > cnt: 
+            cnt = dp[j]
+        elif a[i] == b[j]: 
+            dp[j] = cnt + 1
+print(max(dp))
