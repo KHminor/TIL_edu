52923bb61d802c0e26957b23a2a59ec8db736202
KHminor <ghdals2020@gmail.com>
2023-09-29 18:16:49 +0900

    algo

---
+++ Python_new_method/algo/집주소.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    n = input().rstrip('\n')
+    cnt = 0
+    if n == '0': break
+    else: 
+        for i in n:
+            if i == '1': cnt += 2
+            elif i == '0': cnt += 4
+            else: cnt += 3
+        print(cnt + len(n)-1 + 2)
+
+# 2+3+4 = 9+2+2 = 13
