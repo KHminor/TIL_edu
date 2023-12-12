d9db6bee499241143dc3863018928ef129bd7790
KHminor <ghdals2020@gmail.com>
2023-12-12 14:26:21 +0900

    algo

---
+++ Python_new_method/algo/Java/_2312/저항.java
@@ -0,0 +1,41 @@
+package _2312;
+
+import java.io.*;
+import java.util.*;
+
+public class 저항 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        Map<String,Integer[]> map = Map.of(
+//                "black", new Integer[]{0,1},
+//                "brown", new Integer[]{1,10},
+//                "red", new Integer[]{2,100},
+//                "orange", new Integer[]{3,1000},
+//                "yellow", new Integer[]{4,10000},
+//                "green", new Integer[]{5,100000},
+//                "blue", new Integer[]{6,1000000},
+//                "violet", new Integer[]{7,10000000},
+//                "grey", new Integer[]{8,100000000},
+//                "white", new Integer[]{9,1000000000}
+//        );
+        Map<String,Integer[]> map = new HashMap<>();
+        map.put("black", new Integer[]{0,1});
+        map.put("brown", new Integer[]{1,10});
+        map.put("red", new Integer[]{2,100});
+        map.put("orange", new Integer[]{3,1000});
+        map.put("yellow", new Integer[]{4,10000});
+        map.put("green", new Integer[]{5,100000});
+        map.put("blue", new Integer[]{6,1000000});
+        map.put("violet", new Integer[]{7,10000000});
+        map.put("grey", new Integer[]{8,100000000});
+        map.put("white", new Integer[]{9,1000000000});
+        String result = "";
+        for (int i = 0; i < 3; i++) {
+            if (i == 2) {
+                System.out.println((long) Integer.parseInt(result) * map.get(br.readLine())[1]);
+            } else {
+                result = result.concat(map.get(br.readLine())[0].toString());
+            }
+        }
+    }
+}
---
+++ Python_new_method/algo/저항.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+dic = {
+    "black": [0,1],
+    "brown": [1,10],
+    "red": [2,100],
+    "orange": [3,1000],
+    "yellow": [4,10000],
+    "green": [5,100000],
+    "blue": [6,1000000],
+    "violet": [7,10000000],
+    "grey": [8,100000000],
+    "white": [9,1000000000],
+}
+result = ""
+for i in range(3):
+    if i == 2: print(int(result) * dic[input().rstrip("\n")][1])
+    else: result += str(dic[input().rstrip("\n")][0])
