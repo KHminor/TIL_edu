db1ec6840958ba888543498cce04a3e656745ffe
KHminor <ghdals2020@gmail.com>
2024-01-27 12:18:15 +0900

    algo

---
+++ Python_new_method/algo/윷놀이.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(3):
+    o,t = 0,0
+    for i in list(map(int,input().split())):
+        if i == 0: o +=1
+        else: t += 1
+    if t == 4: print("E")
+    else: print(chr(64+o))
