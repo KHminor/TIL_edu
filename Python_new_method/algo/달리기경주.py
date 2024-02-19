6da5e178b4e88e4b0c85c47b79c76ebefc2a0e6f
KHminor <ghdals2020@gmail.com>
2024-02-19 12:54:45 +0900

    algo

---
+++ Python_new_method/algo/달리기경주.py
@@ -0,0 +1,10 @@
+def solution(players, callings):
+    dic = {players[i]:i for i in range(len(players))}
+    for i in callings:
+        idx = dic[i]
+        dic[i] -= 1
+        dic[players[idx-1]] += 1
+        players[idx],players[idx-1] = players[idx-1],players[idx]
+    return sorted(dic,key=dic.get)
+
+print(solution(["mumu", "soe", "poe", "kai", "mine"],["kai", "kai", "mine", "mine"]	))
