7ff05cd65b6d028b917ed785777cfa491acf61f0
KHminor <ghdals2020@gmail.com>
2024-02-22 11:23:18 +0900

    algo

---
+++ Python_new_method/algo/분수의덧셈.py
@@ -0,0 +1,17 @@
+def solution(numer1, denom1, numer2, denom2):
+    
+    def LCM(a,b,gcd): 
+        return b//gcd*a
+    
+    def GCD(a,b):
+        while a != 0:
+            a,b = b%a,a
+        return b
+    
+    lcm = LCM(denom1,denom2, GCD(denom1,denom2))
+    a = lcm//denom1*numer1+lcm//denom2*numer2
+    x = GCD(a,lcm)
+
+    return [a//x,lcm//x]
+
+print(solution(1,2,3,4))
