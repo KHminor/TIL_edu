fb9cc8299a57f5ab42a0285b8bc611efbdfdb547
KHminor <ghdals2020@gmail.com>
2024-02-28 12:05:45 +0900

    algo

---
+++ Python_new_method/algo/크기가작은부분문자열.py
@@ -0,0 +1,7 @@
+def solution(t, p):
+    result,p_ln = 0, len(p)
+    for i in range(len(t)-p_ln+1):
+        if int(p) >= int("".join(t[i:i+p_ln])): result += 1
+    return result
+
+print(solution("3141592","271"))
