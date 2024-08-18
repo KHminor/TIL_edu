6456e9c4d21843cb51f2989fb1099f3cb1efaeca
KHminor <ghdals2020@gmail.com>
2024-08-18 13:44:22 +0900

    java

---
+++ algo/algo/Java/algo_java/_1001.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class _1001 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] li = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        System.out.println(li[0]-li[1]);
+    }
+}
