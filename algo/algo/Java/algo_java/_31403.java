2963a679ec78ecddefd1c1b75a2c43635470a3ba
KHminor <ghdals2020@gmail.com>
2024-10-14 16:27:15 +0900

    algo

---
+++ algo/algo/Java/algo_java/_31403.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+
+public class _31403 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String a = br.readLine();
+        String b = br.readLine();
+        String c = br.readLine();
+        bw.write(Integer.parseInt(a)+Integer.parseInt(b)-Integer.parseInt(c)+"\n");
+        bw.write(Integer.parseInt(a+b)-Integer.parseInt(c)+"\n");
+        bw.flush();
+    }
+}
