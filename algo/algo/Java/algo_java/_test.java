fc8902ee73426bc0e451bfbd94855444b8dbb26f
KHminor <ghdals2020@gmail.com>
2024-12-17 22:59:10 +0900

    algo

--- algo/algo/Java/algo_java/_test.java
+++ algo/algo/Java/algo_java/_test.java
@@ -3,50 +3,20 @@ import java.util.stream.Collectors;
 
 public class _test {
     public static void main(String[] args) {
-//        int[][] li = {
-//                {3, 2},
-//                {1, 5},
-//                {3, 1},
-//                {2, 4},
-//                {1, 2}
-//        };
-//
-//        Arrays.sort(li, new Comparator<int[]>() {
-//            public int compare(int[] o1, int[] o2) {
-//                if (o1[0]==o2[0]) return Integer.compare(o1[1],o2[1]);
-//                return Integer.compare(o1[1],o2[1]);
-//            }
-//        });
-//        int[] ll = {4,5,6,2};
-//        System.out.println(Arrays.stream(ll).max().orElse(-1));
-//
-//        int hap = Arrays.stream(li).flatMapToInt(Arrays::stream).sum();
-//        System.out.println(hap);
-//
-//        String x = "0123456";
-//        System.out.println(x.substring(0));
-        for (int i=2; i>3; i++) System.out.println(i);
+        int[][] li = {
+                {3, 2},
+                {1, 5},
+                {3, 1},
+                {2, 4},
+                {1, 2}
+        };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//        Arrays.sort(li, new Comparator<int[]>() {
-//            public int compare(int[] o1, int[] o2) {
-//                if (o1[0] != o2[0]) return Integer.compare(o1[0],o2[0]);
-//                else return Integer.compare(o1[1],o2[1]);
-//            }
-//        });
+        Arrays.sort(li, new Comparator<int[]>() {
+            public int compare(int[] a1, int[] a2) {
+                if (a1[0]!=a2[0]) return a1[0]-a2[0];
+                else return a2[1]-a1[1];
+            }
+        });
+        for (int[] x: li) System.out.println(Arrays.toString(x));
     }
 }
