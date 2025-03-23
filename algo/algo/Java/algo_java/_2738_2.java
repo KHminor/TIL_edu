71a5ddc1081940c4bc87c5a508e0c26d466ed808
KHminor <ghdals2020@gmail.com>
2025-03-23 23:16:18 +0900

    algo

--- algo/algo/Java/algo_java/_2738_2.java
+++ algo/algo/Java/algo_java/_2738_2.java
@@ -4,16 +4,18 @@ import java.util.*;
 public class _2738_2 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
+    static int n;
+    static int m;
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         int[][] mtx = new int[n][m];
-        mtxAdd(n,m,mtx);
-        printMtx(n,m,mtx);
+        mtxAdd(mtx);
+        printMtx(mtx);
     }
 
-    public static void mtxAdd(int n, int m, int[][] mtx) throws IOException {
+    public static void mtxAdd(int[][] mtx) throws IOException {
         for (int t=0; t<2; t++) {
             for (int i=0; i<n; i++) {
                 st = new StringTokenizer(br.readLine());
@@ -22,7 +24,7 @@ public class _2738_2 {
         }
     }
 
-    public static void printMtx(int n, int m, int[][] mtx) {
+    public static void printMtx(int[][] mtx) {
        StringBuilder sb = new StringBuilder();
         for (int i=0; i<n; i++) {
             for (int j=0; j<m; j++) sb.append(mtx[i][j]).append(" ");
