1dd57c1042601b3774e2ec74e39192a0de5f7f00
KHminor <ghdals2020@gmail.com>
2025-11-24 17:23:20 +0900

    new algo 25.11.24

---
+++ algo/algo/_2511/bj_9093.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class bj_9093 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb;
+        int T = Integer.parseInt(br.readLine());
+        while (T!=0) {
+            String[] arr = br.readLine().split(" ");
+            sb = new StringBuilder();
+            for (int i=0; i<arr.length; i++) {
+                for (int j=arr[i].length()-1; j>=0; j--) {
+                    sb.append(arr[i].charAt(j));
+                }
+                sb.append(" ");
+            }
+            bw.write(sb+"\n");
+            T--;
+        }
+        bw.flush();
+        bw.close();
+    }
+}
